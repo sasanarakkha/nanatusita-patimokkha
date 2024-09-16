49deeb10c14a952775244ce2d3ce046a54811893
paladhammika <paladhammika@protonmail.com>
2024-09-16 19:49:03 +0800

    styles were missing

--- assets/bin/increment-sections.py
+++ assets/bin/increment-sections.py
@@ -21,7 +21,7 @@ with open(file_path, 'r', encoding='utf-8') as file:
     content = file.read()
 
 # Initialize the counter
-counter = 494
+counter = 1
 
 # Function to replace the number incrementally
 def replace_numbers(match):
